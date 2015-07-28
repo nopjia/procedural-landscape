a6e95f3322db0905aa4fbfe83889b4bf8b3f19de
Nop Jiarathanakul <nop@iamnop.com>
2015-07-27 23:57:20 -0700

    fog

--- app/shaders/LandscapeShader.fs.glsl
+++ app/shaders/LandscapeShader.fs.glsl
@@ -1,18 +1,17 @@
 #extension GL_OES_standard_derivatives : enable
 
-#define SIZE 100.0
+varying vec2 vUv;
 
+#define GRID_SIZE 100.0
 #define K_LINE 0.5  // line width
 #define K_SM 4.0    // smooth radius in terms of lineWidth
 
-varying vec2 vUv;
-
-float getGrid(vec2 uv) {
+vec3 getGrid(vec3 color, vec2 uv) {
   vec2 pixSize = abs(dFdx(vUv)) + abs(dFdy(vUv));
 
-  vec2 lineWidth = vec2(K_LINE * SIZE / 2.0) * pixSize;
+  vec2 lineWidth = vec2(K_LINE * GRID_SIZE / 2.0) * pixSize;  // scales with size
 
-  vec2 coords = mod(uv*vec2(SIZE), vec2(1.0));
+  vec2 coords = mod(uv*vec2(GRID_SIZE), vec2(1.0));
   coords.x = coords.x > 0.5 ? 1.0-coords.x : coords.x;
   coords.y = coords.y > 0.5 ? 1.0-coords.y : coords.y;
 
@@ -20,17 +19,28 @@ float getGrid(vec2 uv) {
   vec2 line2 = smoothstep(0.0, 1.0,
     -1.0/((K_SM-1.0)*lineWidth) * coords +
     K_SM/(K_SM-1.0));
-  return max(line2.x, line2.y);
+  return color * max(line2.x, line2.y);
 
   // float line = 0.0;
   // if (any(lessThan(coords, vec2(lineWidth))))
   //   line = 1.0;
-  // return line;
+}
+
+#define K_FOG_COLOR vec3(0.0, 0.0, 0.0)
+#define K_FOG_DENSITY 0.03
+
+vec3 getFog(vec3 color) {
+  float depth = gl_FragCoord.z / gl_FragCoord.w;
+  float fogFactor = exp2(-K_FOG_DENSITY*K_FOG_DENSITY * depth*depth * 1.442695);
+  fogFactor = clamp(1.0 - fogFactor, 0.0, 1.0);
+  return mix(color, K_FOG_COLOR, fogFactor);
 }
 
 void main() {
   vec3 color = vec3(1.0, 0.0, 0.0);
 
-  color *= getGrid(vUv);
+  color = getGrid(color, vUv);
+  color = getFog(color);
+
   gl_FragColor = vec4(color, 1.0);
 }
